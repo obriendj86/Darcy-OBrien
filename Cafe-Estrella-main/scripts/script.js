f8cce335f905dcbe37261a845129537c92cc360c
obriendj86 <darcy.obrien4@gmail.com>
2025-02-10 18:01:20 -0600

    Add files via upload

---
+++ Cafe-Estrella-main/scripts/script.js
@@ -0,0 +1,9 @@
+/*
+Author: Darcy O'Brien
+File Name: script.js
+Date: 4/19/2024
+*/
+
+function hamburger() {
+var menu = document.getElementById("menu-links"); if (menu.style.display === "block") { menu.style.display = "none";
+} else { menu.style.display = "block"; } }
